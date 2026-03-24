013f770e216d754cd27494bd10e674fd111fe937
a7medmo7amady <ahmedmohamady2005@gmail.com>
2026-03-24 17:00:25 +0200

    A Bug in the passport

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,23 +1,32 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
-
+import User from "../models/User";
 
 passport.use(
   new GoogleStrategy(
-   {
-  clientID: process.env.GOOGLE_CLIENT_ID!,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  callbackURL: process.env.BASE_UR!,
-},
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.BASE_URL!,
+    },
     async (_accessToken, _refreshToken, profile: Profile, done) => {
       try {
-        // TODO: find or create user in your DB
-        const user = {
-          id: profile.id,
-          name: profile.displayName,
-          email: profile.emails?.[0].value,
-          avatar: profile.photos?.[0].value,
-        };
+        const email = profile.emails?.[0]?.value;
+        if (!email) return done(new Error("No email returned from Google"), undefined);
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          const username = email.split("@")[0] + "_" + profile.id.slice(0, 6);
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            username,
+            password: "google-oauth-" + profile.id,
+            emailVerified: true,
+          });
+        }
+
         return done(null, user);
       } catch (err) {
         return done(err);
